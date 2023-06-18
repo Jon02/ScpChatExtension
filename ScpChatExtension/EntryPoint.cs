656283146a6517f3a0929d8797410e2ff378f4cb
Jon02 <56976411+Jon02@users.noreply.github.com>
2023-06-18 12:53:53 +0200

    Update version and info

--- ScpChatExtension/EntryPoint.cs
+++ ScpChatExtension/EntryPoint.cs
@@ -12,10 +12,10 @@ namespace ScpChatExtension;
 
 public class EntryPoint : Plugin<PluginConfig>
 {
-    public override string Author { get; } = "warden161 (original by Jesus-QC)";
+    public override string Author { get; } = "jonny02 (original by Jesus-QC and warden161)";
     public override string Name { get; } = "ScpChatExtensions";
     public override Version RequiredExiledVersion { get; } = new(7, 0, 0);
-    public override Version Version { get; } = new(1, 1, 0);
+    public override Version Version { get; } = new(1, 2, 0);
 
     public static Harmony HarmonyPatcher { get; private set; }
     public static List<ReferenceHub> ProximityToggled { get; set; } = new List<ReferenceHub>();
